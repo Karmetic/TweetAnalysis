5654ca945fd613feb2c129294a569c777e8f8714
Karmetic <IMFields03@gmail.com>
2021-09-24 11:38:36 -0500

    Created base tweet class, not finished, simply made it exist

--- TweetAnalysis/Tweet.java
+++ TweetAnalysis/Tweet.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
 
-public class Tweet {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Tweet implements Comparable<Tweet>{
+		private long tweet_id;
+		private int polarity;
+		private String user_name;
+		private ArrayList<String> tweet_text;
+		
+		public Tweet(int polar, long id, String user) {
+			polarity = polar;
+			tweet_id = id;
+			user_name = user;
+			tweet_text = new ArrayList<String>();
+		}
+		
+		public int compareTo(Tweet o) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+		
 	}
 
 }
